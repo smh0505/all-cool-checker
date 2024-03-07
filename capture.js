df6805872f3d373c63695976a8b8fb209ee5c98e
Bloppy <mson9705@gmail.com>
2024-03-06 23:45:48 -0500

    Optimized CPU usage

--- capture.js
+++ capture.js
@@ -8,11 +8,16 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 ctx.filter = "contrast(1.4)";
 
-const startBtn = document.querySelector("#startBtn")
+const startBtn = document.querySelector("#startBtn");
+
+const worker = await Tesseract.createWorker("eng");
+await worker.setParameters({
+    tessedit_char_whitelist: "0123456789",
+});
 
 startBtn.onclick = async () => {
     try {
-        startBtn.setAttribute("disabled", "true")
+        startBtn.setAttribute("disabled", "true");
         const stream = await navigator.mediaDevices.getDisplayMedia({
             video: {
                 displaySurface: "window",
@@ -28,28 +33,19 @@ startBtn.onclick = async () => {
             canvas.height = height.value;
             ctx.drawImage(video, xpos.value, ypos.value, width.value, height.value, 0, 0, width.value, height.value);
             await recognize();
-            target = requestAnimationFrame(drawFrame);
+            requestAnimationFrame(drawFrame);
         };
 
         drawFrame();
     } catch (err) {
         console.error(err);
-        startBtn.removeAttribute("disabled")
+        startBtn.removeAttribute("disabled");
     }
 };
 
 async function recognize() {
-    const worker = await Tesseract.createWorker("eng");
-
-    (async () => {
-        await worker.setParameters({
-            tessedit_char_whitelist: "0123456789",
-        });
-
-        const {
-            data: { text },
-        } = await worker.recognize(canvas);
-        document.querySelector("#log").innerHTML = text;
-        await worker.terminate();
-    })();
+    const {
+        data: { text },
+    } = await worker.recognize(canvas);
+    document.querySelector("#log").innerHTML = text;
 }
